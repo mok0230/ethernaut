951afb08e6531c88e23d7def37c606e2c35b31c1
mok <mok0230@protonmail.com>
2021-12-19 20:30:45 -0600

    update deploy script to point to telephone contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,7 +7,7 @@ async function main() {
   
   console.log("Account balance:", (await ethers.utils.formatEther(weiAmount)));
 
-  const Contract = await ethers.getContractFactory("Greeter");
+  const Contract = await ethers.getContractFactory("Telephone");
   const contract = await Contract.deploy();
 
   console.log("Contract address:", contract.address);
